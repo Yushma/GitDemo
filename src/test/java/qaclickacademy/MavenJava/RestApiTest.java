b81fdef3622357d11abe8508e05e15e8f3adb67a
Yushma <yushmaprem.rao@gmail.com>
2021-08-27 02:09:34 +0530

    First commit

---
+++ src/test/java/qaclickacademy/MavenJava/RestApiTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+//TestNG xml file from maven
+	//How to run all test cases from Test folder mvn test
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
